a7f263b945d2e63ddc4855d5e7b1ab36ff3942ef
larsgrim <lars.grimstad@nmbu.no>
2019-07-25 17:31:20 +0200

    added crc16 algorithm

--- arduino/SerialMotor/SerialMotor.ino
+++ arduino/SerialMotor/SerialMotor.ino
@@ -58,6 +58,37 @@ int calculateCommand(int current_speed, int target_speed)
   
 }
 
+
+
+
+// Checksum
+unsigned short crc16(char *data_p, unsigned short length)
+{
+  unsigned char i;
+  unsigned int data;
+  unsigned int crc = 0x0000;
+
+  if (length == 0)
+    return (~crc);
+
+  do
+  {
+    for (i=0, data=(unsigned int)0xff & *data_p++;
+         i < 8; 
+         i++, data >>= 1)
+    {
+          if ((crc & 0x0001) ^ (data & 0x0001))
+                crc = (crc >> 1) ^ POLY;
+          else  crc >>= 1;
+    }
+  } while (--length);
+
+
+  return (crc);
+}
+
+
+
 void setup()
 {
   Serial.begin(115200);
