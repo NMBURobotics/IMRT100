33d8e50c0a09a2ac4900cef439a567c45238ec71
Lars Grimstad <larg@nmbu.no>
2019-07-25 18:20:36 +0200

    started on serial imput

--- arduino/SerialMotor/SerialMotor.ino
+++ arduino/SerialMotor/SerialMotor.ino
@@ -7,12 +7,21 @@
 
 #define CMD_RATE 40
 
+#define MSG_SIZE 10
+
+#define POLY 0x8408
+
 DualVNH5019MotorShield md;
 
 
 unsigned long next_cmd_time_;
 int m1_cmd_ = 0;
 int m2_cmd_ = 0;
+int target_v1_ = 0;
+int target_v2_ = 0;
+char rx_buffer_[MSG_SIZE];
+int next_write_ = 0;
+bool msg_complete_ = false;
 
 
 
@@ -101,20 +110,64 @@ void loop()
 {
     unsigned long current_time = millis();
 
+    if (msg_complete_)
+    {
+      target_v1_ = (int)( (rx_buffer_[1] << 8) | (rx_buffer_[2]) );
+      target_v2_ = (int)( (rx_buffer_[3] << 8) | (rx_buffer_[4]) );
+      msg_complete_ = false;
+    }
+
     if (current_time > next_cmd_time_)
     {
-      m1_cmd_ = calculateCommand(m1_cmd_, 400);
-      m2_cmd_ = calculateCommand(m2_cmd_, 400);
+      m1_cmd_ = calculateCommand(m1_cmd_, target_v1_);
+      m2_cmd_ = calculateCommand(m2_cmd_, target_v2_);
       next_cmd_time_ += 1000 / CMD_RATE;
-      Serial.println(m1_cmd_);
+
     }
     
-    //md.setM1Speed(m1_cmd_);
-    //md.setM2Speed(m2_cmd_);
+    md.setM1Speed(m1_cmd_);
+    md.setM2Speed(m2_cmd_);
     stopIfFault();
     
     
 
 
  
+}
+
+
+void serialEvent()
+{
+  int bytes_read = 0;
+  
+  while (Serial.available() && bytes_read < 100 && !msg_complete_)
+  {
+    char in_char = (char)Serial.read();
+    if (next_write_ == 0)
+    {
+      if (in_char == 'c')
+      {
+        rx_buffer_[next_write_] = in_char;
+        next_write_++;
+      }
+      else
+      {
+        Serial.println("BAD HEADER");
+      }
+    }
+    else
+    {
+      rx_buffer_[next_write_] = in_char;
+      next_write_++;
+    }
+
+    if (next_write_ == MSG_SIZE)
+    {
+      msg_complete_ = true;
+      next_write_ = 0;
+    }
+    
+    
+  }
+
 }
