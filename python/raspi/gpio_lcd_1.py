12a73c6feaf15ceadb5174081069677896e3deb6
larsgrim <larg@nmbu.no>
2021-07-26 18:30:38 +0200

    updated to work with original library

--- python/raspi/gpio_lcd_1.py
+++ python/raspi/gpio_lcd_1.py
@@ -1,13 +1,16 @@
 # This program will require you to set up the Raspberry for I2C communication,
 # install some packages and download a python module. A nice guide can be found at:
 # http://www.circuitbasics.com/raspberry-pi-i2c-lcd-set-up-and-programming/
+#
+# use RPi_I2C_driver.py from the following site rather than from the link above
+# https://gist.github.com/DenisFromHR/cc863375a6e19dce359d
 
-import I2C_LCD_driver
+import RPi_I2C_driver
 import time
 
-my_lcd = I2C_LCD_driver.lcd()
-my_lcd.lcd_display_string("IMRT 100", 1, 4)
-my_lcd.lcd_display_string("Anvendt Robotikk", 2, 0)
+my_lcd = RPi_I2C_driver.lcd()
+my_lcd.lcd_display_string_pos("IMRT 100", 1, 4)
+my_lcd.lcd_display_string_pos("Anvendt Robotikk", 2, 0)
 print("Bye!")
             
 
--- python/raspi/gpio_lcd_2.py
+++ python/raspi/gpio_lcd_2.py
@@ -1,11 +1,13 @@
 # This program will require you to set up the Raspberry for I2C communication,
 # install some packages and download a python module. A nice guide can be found at:
 # http://www.circuitbasics.com/raspberry-pi-i2c-lcd-set-up-and-programming/
+#
+# use RPi_I2C_driver.py from the following site rather than from the link above # https://gist.github.com/DenisFromHR/cc863375a6e19dce359d
 
-import I2C_LCD_driver
+import RPi_I2C_driver   
 import time
 
-my_lcd = I2C_LCD_driver.lcd()
+my_lcd = RPi_I2C_driver.lcd()
 message = ["Hello world!", "How are you?", "My name is..", "ScreenMcScrnface", "I am a LCD"]
 animation = ["^", ">", "v", "<"]
 anim_idx = 0;
@@ -13,7 +15,7 @@ anim_idx = 0;
 try:
     while True:
         # Print headline on top row
-        my_lcd.lcd_display_string("IMRT 100", 1, 4)
+        my_lcd.lcd_display_string_pos("IMRT 100", 1, 4)
 
         # Loop through lines in message
         for line in message:
@@ -23,19 +25,19 @@ try:
 
                 # if index i is a valid index for character in line 
                 if i < len(line):
-                    my_lcd.lcd_display_string(line[i], 2, i)
+                    my_lcd.lcd_display_string_pos(line[i], 2, i)
 
                 # Update animation every second iteration
                 if i%2 ==0:
-                    my_lcd.lcd_display_string(3*animation[anim_idx], 1, 0)
-                    my_lcd.lcd_display_string(3*animation[anim_idx], 1, 13)
+                    my_lcd.lcd_display_string_pos(3*animation[anim_idx], 1, 0)
+                    my_lcd.lcd_display_string_pos(3*animation[anim_idx], 1, 13)
                     anim_idx = (anim_idx + 1) % len(animation)
 
                 # Sleep for 0.1 seconds
                 time.sleep(0.1)
 
             # Clear second row by filling it with whitespace
-            my_lcd.lcd_display_string(16*" ", 2, 0)
+            my_lcd.lcd_display_string_pos(16*" ", 2, 0)
 
 except KeyboardInterrupt:
     print("Terminated by user")
